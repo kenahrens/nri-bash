4c63451b7ef3db46d25fae9a7e004987f8ca23e4
Ken Ahrens <kahrens@newrelic.com>
2017-11-06 17:29:04 -0500

    adding subproject to installer

--- ssl-cert-expire/install.sh
+++ ssl-cert-expire/install.sh
@@ -6,6 +6,7 @@ ETC_CONFIG=/etc/newrelic-infra/integrations.d
 # Create these directories if they don't exist
 mkdir ${CUSTOM_INTEGRATIONS}/bin
 mkdir ${CUSTOM_INTEGRATIONS}/template
+mkdir ${CUSTOM_INTEGRATIONS}/ssl-cert-check
 
 # Copy the config file to the integrations.d directory
 cp ./config/*.yaml $ETC_CONFIG
@@ -26,6 +27,14 @@ echo "ssl-cert-expire script copied."
 # Make sure the executable can be executed
 chmod 755 ${CUSTOM_INTEGRATIONS}/bin/ssl-cert-expire.sh
 echo "ssl-cert-expire made into an executable."
+
+# Copy the shell script the bin directory
+cp ./ssl-cert-check/ssl-cert-check ${CUSTOM_INTEGRATIONS}/ssl-cert-check
+echo "ssl-cert-check subproject copied."
+
+# Make sure the executable can be executed
+chmod 755 ${CUSTOM_INTEGRATIONS}/ssl-cert-check/ssl-cert-check
+echo "ssl-cert-check subproject made into an executable."
 
 # Restart the newrelic-infra agent
 service newrelic-infra restart
